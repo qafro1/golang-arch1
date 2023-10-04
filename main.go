dd2b894feac6426177afdfa93e64df6ae84825c1
qafro1 <19574150+qafro1@users.noreply.github.com>
2023-10-04 14:34:52 +0100

    Added unmarshal

--- main.go
+++ main.go
@@ -28,6 +28,16 @@ func main() {
 		log.Panic(err)
 
 	}
-	fmt.Println(string(bs))
+	fmt.Println("Print JSON", string(bs))
+
+	xp2 := []person{}
+	err = json.Unmarshal(bs, &xp2)
+
+	if err != nil {
+		log.Panic(err)
+
+	}
+
+	fmt.Println("Convert back to Go structure", xp2)
 
 }
